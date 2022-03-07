2fabb6a194ff17d01b3794bbcccfdaa2efb5f568
nhocvukhoa <71363631+nhocvukhoa@users.noreply.github.com>
2022-03-07 17:42:28 +0700

    first commit

---
+++ laravel/framework/src/Illuminate/Session/SessionServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Illuminate\Session;
+
+use Illuminate\Contracts\Cache\Factory as CacheFactory;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\ServiceProvider;
+
+class SessionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerSessionManager();
+
+        $this->registerSessionDriver();
+
+        $this->app->singleton(StartSession::class, function ($app) {
+            return new StartSession($app->make(SessionManager::class), function () use ($app) {
+                return $app->make(CacheFactory::class);
+            });
+        });
+    }
+
+    /**
+     * Register the session manager instance.
+     *
+     * @return void
+     */
+    protected function registerSessionManager()
+    {
+        $this->app->singleton('session', function ($app) {
+            return new SessionManager($app);
+        });
+    }
+
+    /**
+     * Register the session driver instance.
+     *
+     * @return void
+     */
+    protected function registerSessionDriver()
+    {
+        $this->app->singleton('session.store', function ($app) {
+            // First, we will create the session manager which is responsible for the
+            // creation of the various session drivers when they are needed by the
+            // application instance, and will resolve them on a lazy load basis.
+            return $app->make('session')->driver();
+        });
+    }
+
+    protected function buildSession($handler)
+{
+    if ($this->app['config']['session.encrypt']) {
+        return new EncryptedStore(
+            $this->app['config']['session.cookie'], $handler, $this->app['encrypter']
+        );
+    } else {
+        return new Store($this->app['config']['session.cookie'], $handler);
+    }
+}
+
+}
